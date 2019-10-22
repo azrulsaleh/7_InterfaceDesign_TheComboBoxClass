5236db798d1c3bad04150739c5158bbb9a6171e9
Azrul Saleh <azrulsaleh@me.com>
2019-10-22 02:06:02 -0700

    Completed tutorial

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -11,6 +11,38 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+    addAndMakeVisible(textLabel);
+    textLabel.setFont(textFont);
+    
+    addAndMakeVisible(styleMenu);
+    styleMenu.addItem("Plain", stylePlain);
+    styleMenu.addItem("Bold", styleBold);
+    styleMenu.addItem("Italic", styleItalic);
+    styleMenu.onChange = [this] { styleMenuChanged(); };
+    styleMenu.setSelectedId(stylePlain);
+    
+    addAndMakeVisible(coloursMenu);
+    coloursMenu.addItem("Black", black);
+    coloursMenu.addItem("White", white);
+    coloursMenu.addSeparator();
+    coloursMenu.addSectionHeading("Reds");
+    coloursMenu.addItem("Red", red);
+    coloursMenu.addItem("Dark Red", darkred);
+    coloursMenu.addItem("Indian Red", indianred);
+    coloursMenu.addSeparator();
+    coloursMenu.addSectionHeading("Greens");
+    coloursMenu.addItem("Green", green);
+    coloursMenu.addItem("Dark Green", darkgreen);
+    coloursMenu.addItem("Light Green", lightgreen);
+    coloursMenu.addSeparator();
+    coloursMenu.addSectionHeading("Blues");
+    coloursMenu.addItem("Blue", blue);
+    coloursMenu.addItem("Dark Blue", darkblue);
+    coloursMenu.addItem("Light Blue", lightblue);
+    coloursMenu.onChange = [this] { coloursMenuChanged(); };
+    coloursMenu.setSelectedId(black);
+    coloursMenu.setEditableText(true);
+    
     setSize (600, 400);
 }
 
@@ -21,17 +53,90 @@ MainComponent::~MainComponent()
 //==============================================================================
 void MainComponent::paint (Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-
-    g.setFont (Font (16.0f));
-    g.setColour (Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+    textLabel.setBounds(10, 10, getWidth() - 20, 20);
+    styleMenu.setBounds(10, 40, getWidth() - 20, 20);
+    coloursMenu.setBounds(10, 70, getWidth() - 20, 20);
+}
+
+void MainComponent::styleMenuChanged()
+{
+    switch (styleMenu.getSelectedId()) {
+        case stylePlain:
+            setLightColoursEnabled(true);
+            textFont.setStyleFlags(Font::plain);
+            break;
+        case styleBold:
+            if ((coloursMenu.getSelectedId() == white) || (coloursMenu.getSelectedId() == indianred) || (coloursMenu.getSelectedId() == lightgreen) || (coloursMenu.getSelectedId() == lightblue))
+            {
+                coloursMenu.setSelectedId(coloursMenu.getSelectedId() - 1);
+            }
+            setLightColoursEnabled(false);
+            textFont.setStyleFlags(Font::bold);
+            break;
+        case styleItalic:
+            setLightColoursEnabled(true);
+            textFont.setStyleFlags(Font::italic);
+            break;
+    }
+    textLabel.setFont (textFont);
+}
+
+void MainComponent::coloursMenuChanged()
+{
+    Colour textColour;
+    switch (coloursMenu.getSelectedId()) {
+        case black: textColour = Colours::black; break;
+        case white: textColour = Colours::white; break;
+        case red: textColour = Colours::red; break;
+        case darkred: textColour = Colours::darkred; break;
+        case indianred: textColour = Colours::indianred; break;
+        case green: textColour = Colours::green; break;
+        case darkgreen: textColour = Colours::darkgreen; break;
+        case lightgreen: textColour = Colours::lightgreen; break;
+        case blue: textColour = Colours::blue; break;
+        case darkblue: textColour = Colours::darkblue; break;
+        case lightblue: textColour = Colours::lightblue; break;
+        default:
+            textColour = handleColourText();
+            break;
+    }
+    textLabel.setColour (Label::textColourId, textColour);
+}
+
+Colour MainComponent::handleColourText()
+{
+    auto menuText = coloursMenu.getText();
+    auto valueFromHex = menuText.getHexValue32();
+//    coloursMenu.setText(String::toHexString((int) valueFromHex));
+    
+    int hexIsUnique = 0;
+    for (int i = 0; i < coloursMenu.getNumItems(); ++i) {
+        if (coloursMenu.getItemText(i) == coloursMenu.getText())
+        {
+            hexIsUnique = 1;
+        }
+    }
+    if (hexIsUnique == 0)
+    {
+        if (coloursMenu.getNumItems() == 11) {
+            coloursMenu.addSeparator();
+            coloursMenu.addSectionHeading("Custom");
+        }
+        coloursMenu.addItem(String::toHexString((int) valueFromHex), coloursMenu.getNumItems() + 1);
+    }
+    
+    return Colour (valueFromHex);
+}
+
+void MainComponent::setLightColoursEnabled (const bool shouldBeEnabled)
+{
+    coloursMenu.setItemEnabled(white, shouldBeEnabled);
+    coloursMenu.setItemEnabled(indianred, shouldBeEnabled);
+    coloursMenu.setItemEnabled(lightgreen, shouldBeEnabled);
+    coloursMenu.setItemEnabled(lightblue, shouldBeEnabled);
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -18,6 +18,30 @@
 class MainComponent   : public Component
 {
 public:
+    enum FontStyles
+    {
+        stylePlain = 1,
+        styleBold,
+        styleItalic,
+        numberOfStyles
+    };
+    
+    enum TextColours
+    {
+        black = 1,
+        white,
+        red,
+        darkred,
+        indianred,
+        green,
+        darkgreen,
+        lightgreen,
+        blue,
+        darkblue,
+        lightblue,
+        numberOfColours
+    };
+    
     //==============================================================================
     MainComponent();
     ~MainComponent();
@@ -25,11 +49,15 @@ public:
     //==============================================================================
     void paint (Graphics&) override;
     void resized() override;
+    void styleMenuChanged();
+    void coloursMenuChanged();
+    Colour handleColourText();
+    void setLightColoursEnabled (const bool);
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
-
+    Label textLabel { {}, "The quick brown fox jumps over the lazy dog." };
+    Font textFont { 12.0f };
+    ComboBox styleMenu, coloursMenu;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
